e68c96843e7818b9c322736f9e92329ccc922a92
alireza.karampour <alireza.karampour@gmail.com>
2023-11-06 16:31:25 +0330

    feat add list tags

--- lib/html/tags.go
+++ lib/html/tags.go
@@ -32,3 +32,30 @@ func A(text string, href string) *lib.BaseComponent {
 	tag.AddAttributes("href=" + href)
 	return &tag
 }
+
+func Li(child *lib.BaseComponent) *lib.BaseComponent {
+	tag := lib.BaseComponent{}
+	tag.TagName = "li"
+	if child != nil {
+		tag.AddChild(child)
+	}
+	return &tag
+}
+
+func Ul(children ...*lib.BaseComponent) *lib.BaseComponent {
+	tag := lib.BaseComponent{}
+	tag.TagName = "ul"
+	for _, v := range children {
+		tag.AddChild(v)
+	}
+	return &tag
+}
+
+func Ol(children ...*lib.BaseComponent) *lib.BaseComponent {
+	tag := lib.BaseComponent{}
+	tag.TagName = "ol"
+	for _, v := range children {
+		tag.AddChild(v)
+	}
+	return &tag
+}
